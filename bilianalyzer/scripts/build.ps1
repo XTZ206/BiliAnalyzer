c9e4a02a2c0f775d066218a978fc5627f0e0fe79
XTZ206 <ywx2006@outlook.com>
2023-01-19 13:32:39 +0800

    build: 增加exe打包脚本

---
+++ bilianalyzer/scripts/build.ps1
@@ -0,0 +1,16 @@
+./venv/Scripts/activate
+Remove-Item .\dist\BiliAnalyzer -Recurse -Force
+pyinstaller -D .\bilianalyzer\main.py -i .\bilianalyzer\icon\main.png -n BiliAnalyzer -w
+
+# 资源文件夹不存在时创建对应资源文件夹
+if ((Test-Path -Path .\dist\BiliAnalyzer\icon) -eq $false)
+{
+    mkdir -Path .\dist\BiliAnalyzer\icon
+}
+Copy-Item -Path .\bilianalyzer\icon\* -Destination .\dist\BiliAnalyzer\icon -Force
+
+if ((Test-Path -Path .\dist\BiliAnalyzer\docs) -eq $false)
+{
+    mkdir -Path .\dist\BiliAnalyzer\docs
+}
+Copy-Item -Path .\bilianalyzer\docs\* -Destination .\dist\BiliAnalyzer\docs -Force
