426db300eeaa2efc7f45968d7cd5b67f01d3fa4c
XTZ206 <ywx2006@outlook.com>
2023-01-20 20:07:14 +0800

    feat: 添加异常模块

---
+++ bilianalyzer/exceptions.py
@@ -0,0 +1,29 @@
+class AnalyzerException(Exception):
+    """
+    BiliAnalyzer 基类异常。
+    """
+
+    def __init__(self, msg: str = "出现了错误，但是未说明具体原因"):
+        super().__init__(msg)
+        self.msg = msg
+
+    def __str__(self):
+        return self.msg
+
+
+class FileFormatException(AnalyzerException):
+    def __init__(self, msg: str = "文件格式错误"):
+        super(FileFormatException, self).__init__(msg)
+        self.msg = msg
+
+
+class FileNotSelectedException(AnalyzerException):
+    def __init__(self, msg: str = "未指定文件路径错误"):
+        super(FileNotSelectedException, self).__init__(msg)
+        self.msg = msg
+
+
+class CheckingException(AnalyzerException):
+    def __init__(self, msg: str = "参数未检查错误"):
+        super(CheckingException, self).__init__(msg)
+        self.msg = msg
