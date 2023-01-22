b9bd8956cc2629c0f2210fd50ac9942f9778dff0
XTZ206 <ywx2006@outlook.com>
2023-01-22 12:29:53 +0800

    build: 修改代码生成脚本

--- bilianalyzer/scripts/ui2py.ps1
+++ bilianalyzer/scripts/ui2py.ps1
@@ -1,13 +1,13 @@
-.\venv\Scripts\activate
+./venv/Scripts/activate
 
-pyside6-uic.exe .\bilianalyzer\ui\main_window.ui -o .\bilianalyzer\ui\ui_main.py
-pyside6-uic.exe .\bilianalyzer\ui\config_window.ui -o .\bilianalyzer\ui\ui_config.py
-pyside6-uic.exe .\bilianalyzer\ui\about_window.ui -o .\bilianalyzer\ui\ui_about.py
-pyside6-uic.exe .\bilianalyzer\ui\tutorial_window.ui -o .\bilianalyzer\ui\ui_tutorial.py
+pyside6-uic.exe ./bilianalyzer/ui/main.ui -o ./bilianalyzer/ui/ui_main.py
+pyside6-uic.exe ./bilianalyzer/ui/config.ui -o ./bilianalyzer/ui/ui_config.py
+pyside6-uic.exe ./bilianalyzer/ui/about.ui -o ./bilianalyzer/ui/ui_about.py
+pyside6-uic.exe ./bilianalyzer/ui/tutorial.ui -o ./bilianalyzer/ui/ui_tutorial.py
 
-python .\bilianalyzer\scripts\relocate.py .\bilianalyzer\ui\ui_main.py
-python .\bilianalyzer\scripts\relocate.py .\bilianalyzer\ui\ui_config.py
-python .\bilianalyzer\scripts\relocate.py .\bilianalyzer\ui\ui_about.py
-python .\bilianalyzer\scripts\relocate.py .\bilianalyzer\ui\ui_tutorial.py
+python ./bilianalyzer/scripts/relocate.py ./bilianalyzer/ui/ui_main.py
+python ./bilianalyzer/scripts/relocate.py ./bilianalyzer/ui/ui_config.py
+python ./bilianalyzer/scripts/relocate.py ./bilianalyzer/ui/ui_about.py
+python ./bilianalyzer/scripts/relocate.py ./bilianalyzer/ui/ui_tutorial.py
 
 Write-Output "Task Complete!"
