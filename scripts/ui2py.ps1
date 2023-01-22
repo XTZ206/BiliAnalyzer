76ee7315a980260dfe09a3541640875bed5ce862
XTZ206 <ywx2006@outlook.com>
2023-01-22 17:32:18 +0800

    fix: 修复资源定位脚本失效问题

--- scripts/ui2py.ps1
+++ scripts/ui2py.ps1
@@ -5,9 +5,9 @@ pyside6-uic.exe ./bilianalyzer/ui/config.ui -o ./bilianalyzer/ui/ui_config.py
 pyside6-uic.exe ./bilianalyzer/ui/about.ui -o ./bilianalyzer/ui/ui_about.py
 pyside6-uic.exe ./bilianalyzer/ui/tutorial.ui -o ./bilianalyzer/ui/ui_tutorial.py
 
-python ./bilianalyzer/scripts/relocate.py ./bilianalyzer/ui/ui_main.py
-python ./bilianalyzer/scripts/relocate.py ./bilianalyzer/ui/ui_config.py
-python ./bilianalyzer/scripts/relocate.py ./bilianalyzer/ui/ui_about.py
-python ./bilianalyzer/scripts/relocate.py ./bilianalyzer/ui/ui_tutorial.py
+python ./scripts/relocate.py ./bilianalyzer/ui/ui_main.py
+python ./scripts/relocate.py ./bilianalyzer/ui/ui_config.py
+python ./scripts/relocate.py ./bilianalyzer/ui/ui_about.py
+python ./scripts/relocate.py ./bilianalyzer/ui/ui_tutorial.py
 
 Write-Output "Task Complete!"
