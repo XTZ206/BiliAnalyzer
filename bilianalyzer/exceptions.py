6f7b6362a591fa927286affbb1748cbfd80a0ba9
XTZ206 <ywx2006@outlook.com>
2023-01-24 18:16:07 +0800

    fix: 去除无用异常

--- bilianalyzer/exceptions.py
+++ bilianalyzer/exceptions.py
@@ -27,15 +27,3 @@ class FileModeException(AnalyzerException):
     def __init__(self, msg: str = "文件模式错误"):
         super(FileModeException, self).__init__(msg)
         self.msg = msg
-
-
-class StorageException(AnalyzerException):
-    def __init__(self, msg: str = "保存文件时发生错误"):
-        super(StorageException, self).__init__(msg)
-        self.msg = msg
-
-
-class CheckingException(AnalyzerException):
-    def __init__(self, msg: str = "参数未检查错误"):
-        super(CheckingException, self).__init__(msg)
-        self.msg = msg
