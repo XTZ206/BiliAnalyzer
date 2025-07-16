234010b7f1e2d0f5cad6fd420eea19d20f878faa
Anzfans <zhangsuo210622@gmail.com>
2025-07-16 21:56:39 +0800

    performance: clean unnecessary import (#14)
    as well as auto format code with black

--- src/bilianalyzer/__main__.py
+++ src/bilianalyzer/__main__.py
@@ -1,23 +1,13 @@
 import click
-import asyncio
-import json
 
-from bilibili_api import Credential
 from .commands import auth_commands, fetch_commands, analyze_commands
-from .fetch.comments import (
-    Fetcher,
-    save_replies,
-    load_replies,
-    save_video_info,
-    load_video_info,
-)
-from .analyze.comments import CommentAnalyzer, save_analysis
-from .utils import *
+
 
 @click.group()
 def main():
     """BiliAnalyzer: Fetch and Analyze Bilibili Comments"""
-    
+
+
 main.add_command(auth_commands.auth)
 main.add_command(fetch_commands.fetch)
 main.add_command(analyze_commands.analyze)
--- src/bilianalyzer/commands/auth_commands.py
+++ src/bilianalyzer/commands/auth_commands.py
@@ -6,27 +6,23 @@ from ..auth import check, login_from_cookies, logout_operation
 def auth():
     """Authenticate BiliAnalyzer with Bilibili Cookies"""
 
+
 @auth.command()
 def status():
     """Check Authentication Status of BiliAnalyzer"""
     click.echo(check())
 
+
 @auth.command()
 def login():
     """Login and Store Cookies for BiliAnalyzer"""
-    sessdata: str = click.prompt(
-                        "Please enter your sessdata cookie for bilibili: "
-                    )
-    bili_jct: str = click.prompt(
-                        "Please enter your bili_jct cookie for bilibili: "
-                    )
+    sessdata: str = click.prompt("Please enter your sessdata cookie for bilibili: ")
+    bili_jct: str = click.prompt("Please enter your bili_jct cookie for bilibili: ")
     try:
-        credential = login_from_cookies(
-                    sessdata=sessdata, bili_jct=bili_jct
-                        )
+        credential = login_from_cookies(sessdata=sessdata, bili_jct=bili_jct)
     except ValueError as error:
-            click.echo(f"Login Failed: {error}")
-            return                    
+        click.echo(f"Login Failed: {error}")
+        return
     click.echo("BiliAnalyzer Logged In Successfully")
 
 
@@ -35,4 +31,3 @@ def logout():
     """Logout BiliAnalyzer and Remove Stored Cookies"""
     logout_operation()
     click.echo("BiliAnalyzer Logged Out Successfully")
-
